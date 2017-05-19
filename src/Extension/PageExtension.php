cbd5dc7d2f18a492e4015ea704dc3b2b13b6fed0
Loz Calver <lozcalver@bigfork.co.uk>
2017-05-19 16:02:18 +0100

    Fix indentation

--- src/Extension/PageExtension.php
+++ src/Extension/PageExtension.php
@@ -10,15 +10,15 @@ use SiteConfig;
 
 class PageExtension extends Extension
 {
-	/**
-	 * @param ContentController $controller
-	 */
-	public function contentcontrollerInit(ContentController $controller)
-	{
-		$siteConfig = SiteConfig::current_site_config();
-		if ($siteConfig->GTMContainerID) {
-			Requirements::insertHeadTags($siteConfig->renderWith('GoogleTagManagerSnippet'));
-			Requirements::insertHeadTags(RequestFilter::NOSCRIPT_PLACEHOLDER);
-		}
-	}
+    /**
+     * @param ContentController $controller
+     */
+    public function contentcontrollerInit(ContentController $controller)
+    {
+        $siteConfig = SiteConfig::current_site_config();
+        if ($siteConfig->GTMContainerID) {
+            Requirements::insertHeadTags($siteConfig->renderWith('GoogleTagManagerSnippet'));
+            Requirements::insertHeadTags(RequestFilter::NOSCRIPT_PLACEHOLDER);
+        }
+    }
 }
